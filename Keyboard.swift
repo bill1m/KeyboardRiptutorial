b3d74b78e4336aeabf45c6ceda17e1427751543c
Bill Morrison <bill1.morrison1@gmail.com>
2020-10-02 12:23:00 -0700

    It works

---
+++ Keyboard.swift
@@ -0,0 +1,60 @@
+//
+//  Keyboard.swift
+//  KeyboardRiptutorial
+//
+//  Created by Bill Morrison on 10/2/20.
+//
+
+import UIKit
+
+import UIKit
+
+// The view controller will adopt this protocol (delegate)
+// and thus must contain the keyWasTapped method
+protocol KeyboardDelegate: class {
+    func keyWasTapped(character: String)
+}
+
+class Keyboard: UIView {
+    @IBAction func button_1(_ sender: Any) {
+        print("pressed 1")
+    }
+    
+    @IBAction func button_2(_ sender: Any) {
+        print("pressed 2")
+    }
+    @IBAction func button_3(_ sender: Any) {
+        print("pressed 3")
+    }
+    // This variable will be set as the view controller so that
+    // the keyboard can send messages to the view controller.
+    weak var delegate: KeyboardDelegate?
+    
+    // MARK:- keyboard initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initializeSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeSubviews()
+    }
+    
+    func initializeSubviews() {
+        let xibFileName = "Keyboard" // xib extention not included
+        let view = Bundle.main.loadNibNamed(xibFileName, owner: self, options: nil)![0] as! UIView
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    // MARK:- Button actions from .xib file
+    
+    @IBAction func keyTapped(sender: UIButton) {
+        // When a button is tapped, send that information to the
+        // delegate (ie, the view controller)
+        self.delegate?.keyWasTapped(character: sender.titleLabel!.text!) // could alternatively send a tag value
+    }
+    
+}
--- KeyboardRiptutorial/ViewController.swift
+++ KeyboardRiptutorial/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, KeyboardDelegate {
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // initialize custom keyboard
+        let keyboardView = Keyboard(frame: CGRect(x: 0, y: 0, width: 0, height: 300))
+        keyboardView.delegate = self // the view controller will be notified by the keyboard whenever a key is tapped
+        
+        // replace system keyboard with custom keyboard
+        textField.inputView = keyboardView
     }
-
-
+    
+    // required method for keyboard delegate protocol
+    func keyWasTapped(character: String) {
+        textField.insertText(character)
+    }
+    // Do any additional setup after loading the view.
 }
 

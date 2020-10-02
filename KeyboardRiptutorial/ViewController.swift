00da656f9427fa1c8dcd9dfff226d9680301976f
Bill Morrison <bill1.morrison1@gmail.com>
2020-10-02 10:20:26 -0700

    Initial Commit

---
+++ KeyboardRiptutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeyboardRiptutorial
+//
+//  Created by Bill Morrison on 10/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

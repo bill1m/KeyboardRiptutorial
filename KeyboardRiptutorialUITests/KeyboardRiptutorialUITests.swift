52c791419eba8f389fa656394ef258e02b310fe8
Bill Morrison <bill1.morrison1@gmail.com>
2020-10-08 05:02:49 -0700

    Add UI test

---
+++ KeyboardRiptutorialUITests/KeyboardRiptutorialUITests.swift
@@ -0,0 +1,40 @@
+//
+//  KeyboardRiptutorialUITests.swift
+//  KeyboardRiptutorialUITests
+//
+//  Created by Bill Morrison on 10/7/20.
+//
+
+import XCTest
+
+class KeyboardRiptutorialUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.textFields["Enter Suits"].tap()
+        app.staticTexts["♣️"].tap()
+        app.staticTexts["♦️"].tap()
+        app.staticTexts["♥️"].tap()
+        app.staticTexts["♠️"].tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["⏎"]/*[[".buttons[\"⏎\"].staticTexts[\"⏎\"]",".staticTexts[\"⏎\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        XCTAssertEqual((app.textFields["Enter Suits"].value as! String), "♣️♦️♥️♠️")
+        print(app.textFields)
+    }
+}
